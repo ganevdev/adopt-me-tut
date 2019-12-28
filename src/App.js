8c4e1096b6eb7b0cee585c5fd196663ffba6bfbc
Ivan Ganev <iganevru@gmail.com>
2019-12-28 20:24:59 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { Router } from '@reach/router';
+import { Link, Router } from '@reach/router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Details from './Details';
 import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
