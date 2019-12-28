4cad435a21c1552216e955d0dde8058a76b4fcc9
Ivan Ganev <iganevru@gmail.com>
2019-12-28 18:19:52 +0300

    all new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
-      <h1 id="some">Adopt Me!</h1>
+      <h1>Adopt Me!</h1>
       <SearchParams />
     </div>
   );
 };
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
+import Results from './Results';
 
 const SearchParams = () => {
   const [location, updateLocation] = useState('Seattle, WA');
   const [breeds, updateBreeds] = useState([]);
+  const [pets, setPets] = useState([]);
   const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
   const [breed, BreedDropdown, updateBreed] = useDropdown('Breed', '', breeds);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    console.log('animals', animals);
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     updateBreeds([]);
@@ -20,7 +34,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -34,6 +53,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
