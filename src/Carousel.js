d0b394f9a132c128396c4d3860e32c4b6a1f4a72
Ivan Ganev <iganevru@gmail.com>
2019-12-29 05:01:34 +0300

    Carousel and Details

---
+++ src/Carousel.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+  state = {
+    photos: [],
+    active: 0,
+  };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ['http://placecorgi.com/600/600'];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photos, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photos}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photos}
+              className={index === active ? 'active' : ''}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
